1a287656ee6f64767d175ea76c4b64cdb7bf8ac9
mxavier305 <mxavierdealmeida@gmail.com>
2026-05-15 20:29:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import {
   Sparkles
 } from 'lucide-react';
 import { motion } from 'motion/react';
-const HERO_IMAGE = "https://i.imgur.com/Si2vooP.png";
+const HERO_IMAGE = "https://imgur.com/a/BaBn05q";
 
 const CTA_BUY_URL = "https://pay.lowify.com.br/checkout?product_id=XorCrD"; // Target URL for purchase
 
